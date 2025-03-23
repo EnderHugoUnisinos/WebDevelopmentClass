0215c8a27b26726aac1e2de7d6ba46d8b9ea4d14
EnderHugo <enderhugo09@gmail.com>
2025-03-22 21:59:27 -0300

    start of forum

---
+++ personal/forumsystem/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum Home</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <div class="header">
+        <h1 class="main-title">Forum header</h1>
+    </div>
+    <div class="navbar-container">
+        <a href="">Link 1</a>
+        <a href="">Link 2</a>
+        <a href="">Link 3</a>
+        <a href="">Link 4</a>
+    </div>
+    <div class="path-container">
+        <p>Path -> Path</p>
+    </div>
+    <div class="forum-container">
+        <div class="forum-top">
+            <div class="topic-title forum-cell">Topic</div>
+            <div class="reply-count forum-cell">Replies</div>
+            <div class="author-name forum-cell">Author</div>
+            <div class="last-reply forum-cell">Last reply</div>
+        </div>
+        <div class="forum-post">
+            <div class="topic-title forum-cell">Topic name</div>
+            <div class="reply-count forum-cell">2</div>
+            <div class="author-name forum-cell">Ender Hugo</div>
+            <div class="last-reply forum-cell">Sat Mar 22, 1998 08:17 pm</div>
+        </div>
+        <div class="forum-post">
+            <div class="topic-title forum-cell">Topic name</div>
+            <div class="reply-count forum-cell">0</div>
+            <div class="author-name forum-cell">Ender Hugo</div>
+            <div class="last-reply forum-cell">Sat Mar 22, 1998 09:39 pm</div>
+        </div>
+    </div>
+    <div class="footer">
+        <p>Developed by Ender Hugo - All rights reserved (1998).</p>
+    </div>
+</body>
+</html>
